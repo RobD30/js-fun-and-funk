8034a941b6d9de5fb03affc44f913bba88e4961a
Robert Dickerson <rdickeriii@gmail.com>
2017-11-07 09:50:41 -0800

    Refactor: destructuring
    end_of_line

--- clue.js
+++ clue.js
@@ -1,12 +1,1 @@
-var game = {};
-
-game.murderer = '??';
-game['weapons'] = [
-    {type: 'lasers', location: 'lab}'},
-    {type: 'angry cats', location: 'lab'},
-    {type: 'dish soap', location: 'lab'}
-];
-
-game.name = [];
-game.name[0] = 'Miss Scarlet';
-game.name.push('Mr. green');
+const [a, b] = [true, false];
