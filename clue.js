5a79a4dcd2d6be7e4a040c6d41958f40554f7899
Robert Dickerson <rdickeriii@gmail.com>
2017-11-08 08:42:59 -0800

    Refactor: work from first challenge.
    end_of_line

--- clue.js
+++ clue.js
@@ -72,3 +72,25 @@ const videoData = [
         ]
     }
 ];
+
+const _ = {};
+_.filter = function(array, callback) {
+    const results = [];
+
+    array.forEach(item => {
+        if (callback(item) === true) {
+            results.push(item);
+        }
+    });
+
+    return results;
+};
+
+
+// demo filter-fn
+function filterFn(item) {
+    return item.present === true;
+};
+
+
+// from class
